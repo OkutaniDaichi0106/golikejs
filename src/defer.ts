6cb845a7795221d904abf654bdf5963a91caf3da
daichiDeskTop <daichi1616@outlook.jp>
2025-10-28 22:54:43 +0900

    style: format code for consistency and readability

--- src/defer.ts
+++ src/defer.ts
@@ -3,23 +3,23 @@ export type DeferredAsyncFunc = () => Promise<void>;
 export type DeferFunc = (fn: DeferredSyncFunc | DeferredAsyncFunc) => void;
 
 export async function scope(fn: (defer: DeferFunc) => void | Promise<void>): Promise<void> {
-    const task: (DeferredSyncFunc | DeferredAsyncFunc)[] = [];
+	const task: (DeferredSyncFunc | DeferredAsyncFunc)[] = [];
 
-    const defer: DeferFunc = (fn) => {
-        task.push(fn);
-    };
+	const defer: DeferFunc = (fn) => {
+		task.push(fn);
+	};
 
-    try {
-        await fn(defer);
-    } finally {
-        while (task.length > 0) {
-            const fn = task.pop();
-            if (!fn) continue;
-            try {
-                await fn();
-            } catch (e) {
-                console.error(e);
-            }
-        }
-    }
+	try {
+		await fn(defer);
+	} finally {
+		while (task.length > 0) {
+			const fn = task.pop();
+			if (!fn) continue;
+			try {
+				await fn();
+			} catch (e) {
+				console.error(e);
+			}
+		}
+	}
 }
